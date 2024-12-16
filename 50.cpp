c2f16ef63525fce05bad0748c97f772b517d50e4
PratibimbGupta <142393615+championorwhat@users.noreply.github.com>
2024-12-16 13:28:47 +0530

    Update 50.cpp

--- 50.cpp
+++ 50.cpp
@@ -1,1 +1,13 @@
-
+class Solution {
+public:
+    double myPow(double x, int n) {
+        int w;
+        if(n<0 && n>INT_MIN){
+            w=abs(n);
+            return 1/pow(x,w);
+        }
+        else{
+            return pow(x,n);
+        }
+    }
+};
